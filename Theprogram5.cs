4beaa4e7a6a0c22d65d6aab5cf69e8e1e3020907
K-E-R-A-D <39189775+K-E-R-A-D@users.noreply.github.com>
2021-02-23 19:10:59 +0700

    commentcommentcommentcommentcommentcomment

--- Theprogram5.cs
+++ Theprogram5.cs
@@ -1,4 +1,12 @@
-﻿using System;
+/*The fifth version of the Theprogram.cs , and is the latest version used in the latest release.
+This code has an improved, faster extraction method, and also a few of exception handling.
+
+-You are free to modify, even distribute the code, and / or improve the code on your own, even use them for your own projects.
+
+-if you wish to compile, or build the program, only build the program in C# 6 and above (but not sure which .NET version so, preferably the latest one)
+because of use of string interpolations.*/
+
+using System;
 using System.IO;
 using System.Text;
 using System.Collections.Generic;
